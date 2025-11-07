208e6e975f46852bb75efc86c186e545fc73a3b7
Mattias <mattias.ekdahl00@gmail.com>
2025-11-07 15:22:02 +0100

    Add form controller with position memory

---
+++ OnatrixCMS/Controllers/FormController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnatrixCMS.ViewModels;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace OnatrixCMS.Controllers;
+
+public class FormController : SurfaceController
+{
+    public FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+    {
+    }
+
+    public IActionResult HandleCallbackForm(CallbackViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return CurrentUmbracoPage();
+        }
+
+        return RedirectToCurrentUmbracoPage();
+    }
+}
---
+++ OnatrixCMS/wwwroot/js/form.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("submit", (e) => {
+    if (e.target.matches("form"))
+        sessionStorage.setItem("scrollY", ""+window.scrollY)
+})
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollY = sessionStorage.getItem("scrollY")
+    if (scrollY) {
+        window.scrollTo({ top: parseInt(scrollY, 10), behavior: "instant" })
+        sessionStorage.removeItem("scrollY")
+    }
+})
