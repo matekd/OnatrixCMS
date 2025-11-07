2081bb2f21ea1cbe8a2439268ce5a21e349ced79
Mattias <mattias.ekdahl00@gmail.com>
2025-11-07 15:49:28 +0100

    Add callbackForm submission and saving

--- OnatrixCMS/Controllers/FormController.cs
+++ OnatrixCMS/Controllers/FormController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnatrixCMS.Services;
 using OnatrixCMS.ViewModels;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Logging;
@@ -10,19 +11,27 @@ using Umbraco.Cms.Web.Website.Controllers;
 
 namespace OnatrixCMS.Controllers;
 
-public class FormController : SurfaceController
+public class FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, FormSubmissionService formSubmissionService) : SurfaceController(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
 {
-    public FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
-    {
-    }
+    private readonly FormSubmissionService _formSubmissionService = formSubmissionService;
 
     public IActionResult HandleCallbackForm(CallbackViewModel model)
     {
         if (!ModelState.IsValid)
         {
+            // Handle error
             return CurrentUmbracoPage();
         }
 
+        var result = _formSubmissionService.SaveCallbackRequest(model);
+
+        if (!result)
+        {
+            // Handle error
+            return RedirectToCurrentUmbracoPage();
+        }
+
+        // Handle success
         return RedirectToCurrentUmbracoPage();
     }
 }
